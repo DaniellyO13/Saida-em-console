3eef3c2a4a4acc3438bb71e110179f01ec566dda
Larah Beatriz <larahbsb06@gmail.com>
2025-09-28 20:39:18 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Aprendendo novos comandos, e entendendo como cada um funciona.
+
+using System;
+
+    namespace Frases
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.Clear();
+                Console.Write("Olá, ");
+                Console.WriteLine("Como vai?");
+                Console.WriteLine("Pressione uma tecla para continuar..");
+                Console.ReadKey();
+                Console.WriteLine("Obrigado e até a proxima.");
+                Console.WriteLine("Pressione uma tecla para finalizar..");
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+                Console.Clear();
+
+            }
+        
+
+        } 
+    }
+
